517f8377236c341af7e0e932aeaf5b449926d0ea
Benjamin Goza <bengoza@gmail.com>
2019-07-16 15:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {eggs: 3};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
+  return Object.assign({})
 }
 
 updateObjectWithKeyAndValue(recipes, flour, "1 cup");
